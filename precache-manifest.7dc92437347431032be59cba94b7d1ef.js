d6c74cdf37bfba47b156bcecb0a2c7ce2cfbac1c
Brandon Slinkard <slinkardbrandon@gmail.com>
2020-03-30 22:32:03 -0500

    Updates

---
+++ precache-manifest.7dc92437347431032be59cba94b7d1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2ad1424a938a92715c3813c14b16ea",
+    "url": "/runestudy/index.html"
+  },
+  {
+    "revision": "1b31e2bb37c7e90908a0",
+    "url": "/runestudy/static/css/main.97e3dbe5.chunk.css"
+  },
+  {
+    "revision": "73afd62da14d8d4e1923",
+    "url": "/runestudy/static/js/2.c0cf46c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/runestudy/static/js/2.c0cf46c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b31e2bb37c7e90908a0",
+    "url": "/runestudy/static/js/main.095108ab.chunk.js"
+  },
+  {
+    "revision": "637a1a52df071b2bc05f",
+    "url": "/runestudy/static/js/runtime-main.1e2731ae.js"
+  }
+]);
