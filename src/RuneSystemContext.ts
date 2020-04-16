11094db1d3762ca7ed1a6522cad14160e485d666
Brandon Slinkard <slinkardbrandon@gmail.com>
2020-04-15 22:26:25 -0500

    Configure stricter linter rules

--- src/RuneSystemContext.ts
+++ src/RuneSystemContext.ts
@@ -1,4 +1,4 @@
-import React, { Context } from 'react';
+import React from 'react';
 import { RuneSystem } from './classes/RuneSystem';
 
 export const RuneSystemContext = React.createContext<RuneSystem[]>([]);
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,43 +1,43 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 interface IButtonProps {
-  variant?: "primary" | "outlined";
+  variant?: 'primary' | 'outlined';
   text: string;
   onClick?: (...args: any[]) => void;
   classes?: string[];
 }
 
 export const Button = ({
-  variant = "primary",
+  variant = 'primary',
   text,
   onClick,
-  classes = []
+  classes = [],
 }: IButtonProps) => {
   const [classNames, setClassNames] = useState<string>();
   React.useEffect(() => {
     const classList = [
-      "font-semibold",
-      "py-2",
-      "px-4",
-      "rounded-full",
-      ...classes
+      'font-semibold',
+      'py-2',
+      'px-4',
+      'rounded-full',
+      ...classes,
     ];
-    if (variant === "primary") {
-      classList.push("bg-green-800", "hover:bg-green-900", "text-white");
+    if (variant === 'primary') {
+      classList.push('bg-green-800', 'hover:bg-green-900', 'text-white');
     } else {
       classList.push(
-        "bg-transparent",
-        "hover:bg-green-800",
-        "hover:text-white",
-        "text-green-900",
-        "border",
-        "border-solid",
-        "border-green-900"
+        'bg-transparent',
+        'hover:bg-green-800',
+        'hover:text-white',
+        'text-green-900',
+        'border',
+        'border-solid',
+        'border-green-900',
       );
     }
 
-    setClassNames(classList.join(" "));
-  }, [variant]);
+    setClassNames(classList.join(' '));
+  }, [variant, classes]);
 
   return (
     <button onClick={onClick} className={classNames}>
