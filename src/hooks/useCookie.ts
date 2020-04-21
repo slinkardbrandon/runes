497d941c2f7c9e27dbdc256c4fd9d072c34f2cd4
Brandon Slinkard <slinkardbrandon@gmail.com>
2020-04-20 23:22:39 -0500

    Document and improve variable names

--- src/hooks/useCookie.ts
+++ src/hooks/useCookie.ts
@@ -5,16 +5,24 @@ import cookies from 'browser-cookies';
  * useCookie - A hook for easy and magic cookies, I might turn this into an npm lib
  */
 export function useCookie(
-  key: string,
-  initialValue: string,
+  // Yeaaaaaaaaaaaaaah
+  cooKey: string,
+  initialCookieValue: string,
 ): [string, (...args: any[]) => void] {
-  const [item, setInnerValue] = React.useState<string>(() => {
-    return cookies.get(key) || initialValue;
+  const [item, setStateValue] = React.useState<string>(() => {
+    /**
+     * Set the initial state value to the cookie value. If no cookie value
+     * exists, take the value passed to set as the initial cookie value.
+     */
+    return cookies.get(cooKey) || initialCookieValue;
   });
 
+  /**
+   * update the state AND cookie value
+   */
   const setValue = (value: string) => {
-    setInnerValue(value);
-    cookies.set(key, value);
+    setStateValue(value);
+    cookies.set(cooKey, value);
   };
 
   return [item, setValue];
