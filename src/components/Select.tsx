7909686e0ce8443f85ce6105bb3eeaa4d191c169
Brandon Slinkard <slinkardbrandon@gmail.com>
2020-04-20 22:53:52 -0500

    Support value overrides on select component

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React from 'react';
 
 interface ISelectProps {
   options: Array<{ value: string; name: string }>;
   onChange?: (...args: any[]) => void;
   wrapperClasses?: string[];
+  value?: string;
 }
 
 export const Select = (props: ISelectProps) => {
-  const wrapperClass = ["inline-block", "relative"]
+  const wrapperClass = ['inline-block', 'relative']
     .concat(props.wrapperClasses ?? [])
-    .join(" ");
+    .join(' ');
 
   return (
     <div className={wrapperClass}>
       <select
         className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline text-gray-700"
+        value={props.value}
         onChange={props.onChange}
       >
         {props.options.map(({ value, name }) => (
