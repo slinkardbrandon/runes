61de0a0c9e951d91d56078de383a146b98a13c25
Brandon Slinkard <slinkardbrandon@gmail.com>
2020-04-24 23:24:08 -0500

    Start to replace tailwindcss with material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import './assets/main.css';
+import { createMuiTheme } from '@material-ui/core/styles';
 import { RuneSystem } from './classes/RuneSystem';
 import { ElderFuthark } from './classes/ElderFuthark';
 import { RuneSystemContext } from './RuneSystemContext';
@@ -10,26 +10,36 @@ import { FlashCards } from './screens/FlashCards';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { ElderFutharkScreen } from './screens/ElderFutharkScreen';
 import { Home } from './screens/Home';
+import { ThemeProvider } from '@material-ui/core';
 
 function App() {
   const runeSystems: RuneSystem[] = [new ElderFuthark()];
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#276749',
+      },
+    },
+  });
 
   return (
     <RuneSystemContext.Provider value={runeSystems}>
-      <div style={{ minHeight: '100vh' }} className="bg-gray-200">
-        <div className="App h-full flex flex-col">
-          <Router basename="/" hashType="slash">
-            <Header />
+      <ThemeProvider theme={theme}>
+        <div style={{ minHeight: '100vh' }} className="bg-gray-200">
+          <div className="App h-full flex flex-col">
+            <Router basename="/" hashType="slash">
+              <Header />
 
-            <Switch>
-              <Route exact path="/" component={() => <Home />} />
-              <Route path="/flash-cards" component={FlashCards} />
-              <Route path="/elder-futhark" component={ElderFutharkScreen} />
-            </Switch>
-          </Router>
+              <Switch>
+                <Route exact path="/" component={() => <Home />} />
+                <Route path="/flash-cards" component={FlashCards} />
+                <Route path="/elder-futhark" component={ElderFutharkScreen} />
+              </Switch>
+            </Router>
+          </div>
         </div>
-      </div>
-      <Footer />
+        <Footer />
+      </ThemeProvider>
     </RuneSystemContext.Provider>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  makeStyles,
+  createStyles,
+  Theme,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    // flex items-center flex-shrink-0 text-white mr-6
+    flex: {
+      display: 'flex',
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    primary: {
+      display: 'flex',
+      alignItems: 'center',
+      color: '#fff',
+      marginRight: theme.spacing(3),
+      flexGrow: 1,
+    },
+    logo: {
+      height: '2.1rem',
+      marginRight: '.5rem',
+    },
+  }),
+);
 
 export const Header = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <nav className="flex items-center justify-between flex-wrap bg-green-800 p-6">
-        <Link to="/">
-          <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <AppBar position="static">
+      <Toolbar className={classes.flex}>
+        <Link to="/" className={classes.grow}>
+          <div className={classes.primary}>
             <img
               src="https://arbor-software.nyc3.digitaloceanspaces.com/runes/rune-study-logo-white-130x200.png"
               alt="Rune Study Logo"
-              className="h-10 mr-2"
+              className={classes.logo}
             />
 
-            <span className="font-semibold text-xl tracking-tight">
-              RuneStudy
-            </span>
+            <Typography variant="h6">RuneStudy</Typography>
           </div>
         </Link>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        {/* <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
-            <a
-              href="/flash-cards"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            >
-              Flash Cards
-            </a>
-          </div>
-        </div> */}
-      </nav>
-    </div>
+
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 };
